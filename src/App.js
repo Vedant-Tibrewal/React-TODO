239080b64b3c03d0dbd54e2b448280bce205c375
Vedant-Tibrewal <veda.tibrewal@gmail.com>
2021-03-01 18:01:50 +0530

    implemented modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Tasks from "./components/Tasks";
+//import Tasks from "./components/Tasks";
 import About from "./components/About";
 import TasksAssigned from "./components/TasksAssigned";
 import Navigationbar from "./components/Navigationbar";
 import Project from "./components/Project";
 import Model from "./components/Model";
 import Analytics from "./components/Analytics";
+import Import from "./components/Import";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+//import Button from "./components/Button";
 
 const App = () => {
   return (
@@ -16,7 +18,7 @@ const App = () => {
       <Route path="/TasksAssigned" exact component={TasksAssigned} />
       <Route path="/Model" exact component={Model} />
       <Route path="/Analytics" exact component={Analytics} />
-      <Route path="/" exact component={Tasks} />
+      <Route path="/" exact component={Import} />
       <Route path="/about" exact component={About} />
     </Router>
   );
